5c4309a61c22f6e84c0345d70496dba3c6d059e4
yushengc <ntuyusheng@gmail.com>
2019-05-13 18:24:23 +0800

    Updates

---
+++ precache-manifest.17272b5371c93584f7b97ee685884ec2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e141aed0ea92a95d0bd2815705dd96ad",
+    "url": "/testMap3/index.html"
+  },
+  {
+    "revision": "bb9c150941b08a31a142",
+    "url": "/testMap3/static/css/main.c50c7fd5.chunk.css"
+  },
+  {
+    "revision": "0001aefd945735b367b9",
+    "url": "/testMap3/static/js/2.f098eb03.chunk.js"
+  },
+  {
+    "revision": "631ad55583fb91b89a1c",
+    "url": "/testMap3/static/js/3.1d32a947.chunk.js"
+  },
+  {
+    "revision": "0c18d6cdb02fbaa039f8",
+    "url": "/testMap3/static/js/4.727ea69a.chunk.js"
+  },
+  {
+    "revision": "bb9c150941b08a31a142",
+    "url": "/testMap3/static/js/main.9a77b05a.chunk.js"
+  },
+  {
+    "revision": "aeb34fe009379b5eca24",
+    "url": "/testMap3/static/js/runtime~main.5039df54.js"
+  }
+]);
